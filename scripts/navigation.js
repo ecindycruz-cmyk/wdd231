a12c16e5110860ae8c2acaec403aa18d6d11db96
ecindycruz-cmyk <ccruzderomero@byupathway.edu>
2026-01-07 23:56:06 -0600

    I did the learning activities

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const navbuttom = document.querySelector("#ham-btn");
+const navlinks = document.querySelector("#nav-bar");
+
+navbuttom.addEventListener("click", () => {
+    navbuttom.classList.toggle("show");
+    navlinks.classList.toggle("show");
+});
